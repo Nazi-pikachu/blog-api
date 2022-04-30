9a4627d1cd08cde7bec8c1f6eb31327f30226404
Rishabh Jain <jain.rishabhche@gmail.com>
2022-05-01 00:48:49 +0530

    migrated the local port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 8000;
 const dotenv = require("dotenv").config();
 const mongoose = require("mongoose");
 const authRoute = require("./routes/auth");
